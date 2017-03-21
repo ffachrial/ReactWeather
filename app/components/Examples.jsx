14c7befdbe37676d3f30ed6b84a6cbd416798c90
Firman Fachrial <firman.fachrial@intikom.co.id>
2017-03-21 13:52:27 +0700

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,7 +10,10 @@ import React from 'react';
 
 var Examples = () => {
   return (
-    <h3>Examples Component!</h3>
+    <div>
+      <h3>Examples Component!</h3>
+      <p>Welcome to examples page!</p>
+    </div>
   )
 };
 
