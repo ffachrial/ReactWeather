86ed407ff11f7c5d4c692c1f36669a5cca7acfc7
Firman Fachrial <firman.fachrial@intikom.co.id>
2017-03-21 14:31:31 +0700

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 // app used public folder for read application
 app.use(express.static('public'));
 
 // app will listen to port 3000
-app.listen(3000, function () {
-  console.log('Express server is up port 3000');
+app.listen(PORT, function () {
+  console.log('Express server is up port ' + PORT);
 });
